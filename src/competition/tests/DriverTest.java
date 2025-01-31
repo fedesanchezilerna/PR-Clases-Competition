f1d0ccd24222b5dec43fc0d426bc624c23b35c13
Federico Sánchez <fedesanchezvidarte@gmail.com>
2025-01-31 12:13:03 +0100

    Adding Driver test class

--- src/competition/tests/DriverTest.java
+++ src/competition/tests/DriverTest.java
@@ -1,4 +1,17 @@
 package competition.tests;
 
+import competition.Driver;
+
 public class DriverTest {
+    public static void main(String[] args) {
+        Driver driver = new Driver("Lewis Hamilton");
+        System.out.println(driver); // Test toString()
+
+        // Test getters and setters
+        System.out.println("Driver ID: " + driver.getId());
+        System.out.println("Next ID: " + Driver.getIdNext());
+
+        driver.setName("Sebastian Vettel");
+        System.out.println("Updated Driver: " + driver);
+    }
 }
