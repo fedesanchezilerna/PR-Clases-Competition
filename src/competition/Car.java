06f463946ce4553894845814481c5283919d7095
Federico Sánchez <fedesanchezvidarte@gmail.com>
2025-02-05 11:31:57 +0100

    Adding Car class and CarTest class

--- src/competition/Car.java
+++ src/competition/Car.java
@@ -1,4 +1,147 @@
 package competition;
 
 public class Car {
+    private static int nextId = 1;
+    private Driver driver;
+    private String brand;
+    private String model;
+    private short year;
+    private Fuel fuel;
+    private short cc;
+    private byte doors;
+    private byte wheels;
+    private byte seats;
+    private int points;
+    private String id;
+
+    public Car(Driver driver, String brand, String model, short year, Fuel fuel, short cc) {
+        this.driver = driver;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.fuel = fuel;
+        this.cc = cc;
+        this.doors = 3;
+        this.wheels = 5;
+        this.seats = 4;
+        this.points = 0;
+        this.id = generateId();
+    }
+
+    public Car(Driver driver, String brand, String model, short year, Fuel fuel, short cc, byte doors, byte wheels, byte seats, int points) {
+        this.driver = driver;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.fuel = fuel;
+        this.cc = cc;
+        this.doors = doors;
+        this.wheels = wheels;
+        this.seats = seats;
+        this.points = points;
+        this.id = generateId();
+    }
+
+    private String generateId() {
+        String code = "CAR" + String.format("%05d", nextId++);
+        String x = brand.substring(0, 1).toUpperCase();
+        String y = model.substring(0, 1).toUpperCase();
+        return code + x + y;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+        this.id = generateId(); // Update ID if the Brand is changed
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+        this.id = generateId();
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public Fuel getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(Fuel fuel) {
+        this.fuel = fuel;
+    }
+
+    public short getCc() {
+        return cc;
+    }
+
+    public void setCc(short cc) {
+        this.cc = cc;
+    }
+
+    public byte getDoors() {
+        return doors;
+    }
+
+    public void setDoors(byte doors) {
+        this.doors = doors;
+    }
+
+    public byte getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(byte wheels) {
+        this.wheels = wheels;
+    }
+
+    public byte getSeats() {
+        return seats;
+    }
+
+    public void setSeats(byte seats) {
+        this.seats = seats;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public static String getIdNext() {
+        return "CAR" + String.format("%05d", nextId);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-10s %-12s %-8s %-6d %-8s %-6d %-3d %-3d %-3d %-3d %-20s %s",
+                id, brand, model, year, fuel, cc, doors, wheels, seats, points, driver.getName(), driver.getId());
+    }
 }
--- src/competition/tests/CarTest.java
+++ src/competition/tests/CarTest.java
@@ -1,4 +1,30 @@
 package competition.tests;
 
+import competition.Car;
+import competition.Driver;
+import competition.Fuel;
+
 public class CarTest {
+    public static void main(String[] args) {
+        Driver driver1 = new Driver("Lewis Hamilton");
+        Driver driver2 = new Driver("Fernando Alonso");
+
+        Car car1 = new Car(driver1, "Mercedes", "W11", (short) 2010, Fuel.HYBRID, (short) 1600);
+        Car car2 = new Car(driver2, "Ferrari", "SF1", (short) 2010, Fuel.HYBRID, (short) 1600, (byte) 2, (byte) 2, (byte) 1, 0);
+
+        System.out.println(car1);
+        System.out.println(car2);
+
+        car1.setBrand("Mercedes RT");
+        System.out.println("\nCar 1 Brand updated: " + car1.getBrand());
+        System.out.println("Car 1 ID updated: " + car1.getId());
+        car1.setModel("W12");
+        System.out.println("\nCar 1 Model updated: " + car1.getModel());
+        System.out.println("Car 1 ID updated: " + car1.getId());
+        System.out.println(car1);
+        System.out.println(car1);
+        System.out.println(car1);
+
+        System.out.println("Next Car ID: " + Car.getIdNext());
+    }
 }
